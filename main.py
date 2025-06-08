c7d47db606c61e1d51da957cd954b4dded850dec
AndresSalazar43822 <andres.salazar43822@ucaldas.edu.co>
2025-06-08 14:04:09 -0500

    Integrar menú principal con módulos de juego y validador AFD

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout,
                             QPushButton, QLabel, QFrame)
 from PyQt6.QtCore import Qt, QSize
-from PyQt6.QtGui import QIcon, QFont, QPixmap
+from PyQt6.QtGui import QIcon, QFont
 from juego_mision.juego import JuegoMision
+from afds.interfaz import InterfazAFD
 
 class MenuPrincipal(QMainWindow):
     def __init__(self):
@@ -47,6 +48,12 @@ class MenuPrincipal(QMainWindow):
             #boton-juego:hover {
                 background-color: #0066CC;
             }
+            #boton-analizador {
+                background-color: #5856D6;
+            }
+            #boton-analizador:hover {
+                background-color: #4744CA;
+            }
             #boton-salir {
                 background-color: #FF3B30;
             }
@@ -72,7 +79,7 @@ class MenuPrincipal(QMainWindow):
         layout.addWidget(titulo)
         
         # Descripción
-        descripcion = QLabel("¡Embárcate en una misión interestelar!")
+        descripcion = QLabel("Seleccione el módulo que desea ejecutar")
         descripcion.setObjectName("descripcion")
         descripcion.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(descripcion)
@@ -83,13 +90,21 @@ class MenuPrincipal(QMainWindow):
         separador.setStyleSheet("background-color: #333333;")
         layout.addWidget(separador)
         
-        # Botón del juego
+        # Botón Juego
         btn_juego = QPushButton("  Misión Interestelar")
         btn_juego.setObjectName("boton-juego")
         btn_juego.setIcon(QIcon("assets/icons/rocket.png"))
         btn_juego.setIconSize(QSize(32, 32))
         btn_juego.clicked.connect(self.iniciar_juego)
         layout.addWidget(btn_juego)
+
+        # Botón AFDs
+        btn_analizador = QPushButton("  Validador AFDs")
+        btn_analizador.setObjectName("boton-analizador")
+        btn_analizador.setIcon(QIcon("assets/icons/code.png"))
+        btn_analizador.setIconSize(QSize(32, 32))
+        btn_analizador.clicked.connect(self.iniciar_analizador)
+        layout.addWidget(btn_analizador)
         
         # Botón Salir
         btn_salir = QPushButton("  Salir")
@@ -111,6 +126,13 @@ class MenuPrincipal(QMainWindow):
         self.juego.ejecutar()
         self.close()
 
+    def iniciar_analizador(self):
+        """Inicia el validador de AFDs"""
+        self.hide()
+        self.analizador = InterfazAFD()
+        self.analizador.ejecutar()
+        # self.close()  # No cierres la ventana principal aquí
+
     def salir(self):
         """Cierra completamente la aplicación"""
         QApplication.quit()
@@ -128,4 +150,4 @@ def main():
     sys.exit(app.exec())
 
 if __name__ == "__main__":
-    main() 
+    main()
